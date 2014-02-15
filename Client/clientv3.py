cad1ad54573b80c7d1864530ad8e5852372d8d4e
FredericIV <alex.borries@gmail.com>
2014-02-15 01:16:20 -0500

    Stableish
    Final clause added, clients exit nicer, server responds better to bad
    closings, further updating to 3.3, client has server selection.

--- Client/clientv3.py
+++ Client/clientv3.py
@@ -1,48 +1,55 @@
-#Jeanette
-
 import socket               # Import socket module
 import _thread
 import tkinter
 
 hostname = socket.gethostname()
-
-print("Type exit to exit")
-s = socket.socket()         # Create a socket object
 try:
-   s.connect(('alarmpi', 4510))
-except socket.error as msg:
-   print("Fail on connection.")
-   input("")
-   exit()
-e = socket.socket()
-e.connect(('alarmpi', 4511))
-def r1(randomis, threadname):
-   while True:
+   print("Type exit to exit")
+   s = socket.socket()         # Create a socket object
+   go = input("Where would you like to connect to? ")
+   try:
+      s.connect((go, 4510))
+   except socket.error as msg:
+      print("Fail on connection.")
+      input("")
+      exit()
+   e = socket.socket()
+   e.connect((go, 4511))
+   def r1(randomis, threadname):
+      while True:
+         try:
+            got = str(e.recv(1024), "utf-8")
+         except socket.error as msg:
+            exit()
+         if not got:
+            sys.exit(1)
+         print(got)
+   def send():
+      if E1.get() == "exit":
+         s.sendall(bytes("exit", "utf-8"))
+         s.close()
+         e.close()
+         exit()
       try:
-         got = e.recv(1024)
+         loss = E1.get()
+         google = str(hostname + "-- " + loss)
+         s.sendall(bytes(google, "utf-8"))
       except socket.error as msg:
          exit()
-      if not got:
-         sys.exit(1)
-      print((got + "\a"))
-def send():
-   if E1.get() == "exit":
-      s.send("exit")
-      s.close()
-      e.close()
-      exit()
-   try:
-      loss = E1.get()
-      s.send(hostname + "-- " + loss)
-   except socket.error as msg:
-      exit()
-_thread.start_new_thread(r1, ("world", "FredericIV"))
-top = tkinter.Tk()
-top.title("Chat Entry")
-L1 = tkinter.Label(top, text="Say:")
-L1.pack( side = "left" )
-E1 = tkinter.Entry(top, relief="flat")
-B = tkinter.Button(top, text ="Send", command = send)
-B.pack( side = "right" )
-E1.pack( fill="both" )
-top.mainloop()
+   _thread.start_new_thread(r1, ("world", "FredericIV"))
+   top = tkinter.Tk()
+   top.title("Chat Entry")
+   L1 = tkinter.Label(top, text="Say:")
+   L1.pack( side = "left" )
+   E1 = tkinter.Entry(top, relief="flat")
+   B = tkinter.Button(top, text ="Send", command = send)
+   B.pack( side = "right" )
+   E1.pack( fill="both" )
+   top.mainloop()
+finally:
+   s.sendall(bytes("exit", "utf-8"))
+   s.shutdown(socket.SHUT_RDWR)
+   s.close()
+   e.shutdown(socket.SHUT_RDWR)
+   e.close()
+   exit()
--- Server/serverv3.py
+++ Server/serverv3.py
@@ -3,80 +3,87 @@ import _thread
 import time
 import queue
 
-print("Running...")
 
-s = socket.socket()
-s.bind(( '', 4510))
-s.listen(4)
+try:
+   print("Running...")
 
-t = socket.socket()
-t.bind(( '', 4511))
-t.listen(4)
+   s = socket.socket()
+   s.bind(( '', 4510))
+   s.listen(4)
 
-messages = queue.Queue()
-messages1 = queue.Queue()
-messages2 = queue.Queue()
-messages3 = queue.Queue()
-messages4 = queue.Queue()
-messagesServer = queue.Queue()
+   t = socket.socket()
+   t.bind(( '', 4511))
+   t.listen(4)
 
-def r1(threadname, port):
-   while True:
-      c, addr = s.accept()
+   messages = queue.Queue()
+   messages1 = queue.Queue()
+   messages2 = queue.Queue()
+   messages3 = queue.Queue()
+   messages4 = queue.Queue()
+   messagesServer = queue.Queue()
+
+   def r1(threadname, port):
       while True:
-         try:
-            got = c.recv(1024)
-         except socket.error as msg:
-            c.close
-         if got == "exit":
-            c.close()
-            break
-         if not got:
-            print ("\a")
-         messages1.put(got)
-         messages2.put(got)
-         messages3.put(got)
-         messages4.put(got)
-         messagesServer.put(got)
-def s1(threadname, port):
-   while True:
-      d, addr = t.accept()
+         c, addr = s.accept()
+         while True:
+            try:
+               got = str(c.recv(1024), "utf-8")
+            except socket.error as msg:
+               c.close()
+            if got == "exit":
+               c.close()
+               break
+            if not got:
+               print ("\a")
+            messages1.put(got)
+            messages2.put(got)
+            messages3.put(got)
+            messages4.put(got)
+            messagesServer.put(got)
+   def s1(threadname, port):
       while True:
-         try:
-            d.send(messages1.get(True))
-         except socket.error as msg:
-            d.close()
-def s2(threadname, port):
-   while True:
-      d, addr = t.accept()
+         d, addr = t.accept()
+         while True:
+            try:
+               d.sendall(bytes(messages1.get(True), "utf-8"))
+            except socket.error as msg:
+               d.close()
+   def s2(threadname, port):
       while True:
-         try:
-            d.send(messages2.get(True))
-         except socket.error as msg:
-            d.close()
-def s3(threadname, port):
-   while True:
-      d, addr = t.accept()
+         d, addr = t.accept()
+         while True:
+            try:
+               d.sendall(bytes(messages2.get(True), "utf-8"))
+            except socket.error as msg:
+               d.close()
+   def s3(threadname, port):
       while True:
-         try:
-            d.send(messages3.get(True))
-         except socket.error as msg:
-            d.close()
-def s4(threadname, port):
-   while True:
-      d, addr = t.accept()
+         d, addr = t.accept()
+         while True:
+            try:
+               d.sendall(bytes(messages3.get(True), "utf-8"))
+            except socket.error as msg:
+               d.close()
+   def s4(threadname, port):
       while True:
-         try:
-            d.send(messages4.get(True))
-         except socket.error as msg:
-            d.close()
-_thread.start_new_thread(r1, ("Felicity", 4510))
-_thread.start_new_thread(r1, ("Jeanette", 4510))
-_thread.start_new_thread(r1, ("Drew", 4510))
-_thread.start_new_thread(r1, ("Frederic", 4510))
-_thread.start_new_thread(s1, ("Felicity", 4511))
-_thread.start_new_thread(s2, ("Jeanette", 4511))
-_thread.start_new_thread(s3, ("Drew", 4511))
-_thread.start_new_thread(s4, ("Frederic", 4511))
-while True:
-   print(messagesServer.get(True))
+         d, addr = t.accept()
+         while True:
+            try:
+               d.sendall(bytes(messages.get(True), "utf-8"))
+            except socket.error as msg:
+               d.close()
+   _thread.start_new_thread(r1, ("Felicity", 4510))
+   _thread.start_new_thread(r1, ("Jeanette", 4510))
+   _thread.start_new_thread(r1, ("Drew", 4510))
+   _thread.start_new_thread(r1, ("Frederic", 4510))
+   _thread.start_new_thread(s1, ("Felicity", 4511))
+   _thread.start_new_thread(s2, ("Jeanette", 4511))
+   _thread.start_new_thread(s3, ("Drew", 4511))
+   _thread.start_new_thread(s4, ("Frederic", 4511))
+   while True:
+      print(messagesServer.get(True))
+finally:
+   s.shutdown(socket.SHUT_RDWR)
+   s.close()
+   t.shutdown(socket.SHUT_RDWR)
+   t.close()
